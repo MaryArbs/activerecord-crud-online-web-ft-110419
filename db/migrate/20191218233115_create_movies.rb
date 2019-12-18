1caae2f01c2204493fdcf32613ca8cc8078b42ab
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-12-18 23:34:43 +0000

    Automatically backed up by Learn

--- db/migrate/20191218233115_create_movies.rb
+++ db/migrate/20191218233115_create_movies.rb
@@ -1,8 +1,13 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
+      def change
+    create_table :movies do |t|
       t.string :title
-      t.string :release_date
-
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
+    end
   end
 end
